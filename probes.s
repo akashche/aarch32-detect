b4ee570b0ccd502275e2acc6126174b6e1379edc
akashche <akashche@redhat.com>
2016-06-06 21:38:45 +0000

    remove sync from probes

--- probes.s
+++ probes.s
@@ -5,7 +5,6 @@
 .global probe_mov
 .type probe_mov, %function
 probe_mov:
-    dsb sy
     push {r4, r5, r6, r7, r8, r9, r10, r11, lr}
     /* mov r0, #0 */
     .word 0xe1a00000
@@ -15,7 +14,6 @@ probe_mov:
 .global probe_udf
 .type probe_udf, %function
 probe_udf:
-    dsb sy
     push {r4, r5, r6, r7, r8, r9, r10, r11, lr}
     /* udf #0 */
     .word 0xe7f000f0
@@ -25,7 +23,6 @@ probe_udf:
 .global probe_ldrex
 .type probe_ldrex, %function
 probe_ldrex:
-    dsb sy
     push {r4, r5, r6, r7, r8, r9, r10, r11, lr}
     /* ldrex r1, [r0] */
     .word 0xe1901f9f
